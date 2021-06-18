45c878995dbdb410ae50e4108aee7e9cc4b73232
Marc Bernard <59966492+mbtools@users.noreply.github.com>
2021-06-18 14:37:23 +0200

    Update descriptions.sh

--- scripts/descriptions.sh
+++ scripts/descriptions.sh
@@ -8,6 +8,6 @@ xmlfiles="$1/*.clas.xml $1/*.intf.xml"
 
 for xmlfile in $xmlfiles
 do
-  echo "Adding MBT prefix to description in $xmlfile"
-  sed -i "0,/^(.*\<DESCRIPT\>)(.*)$/ s/^(.*\<DESCRIPT\>)(.*)$/\1MBT \2/" $xmlfile
+  echo "Adding prefix to description in $xmlfile"
+  sed -i '0,/<DESCRIPT>/ s|^\(.*<DESCRIPT>\)\(.*\)\(</DESCRIPT>\)|\1MBT \2\3|g' $xmlfile
 done
