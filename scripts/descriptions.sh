d6be3e3e71fc322a9647bf99668815b5ebaec6a9
Marc Bernard <59966492+mbtools@users.noreply.github.com>
2021-06-21 14:03:28 +0200

    Update descriptions.sh

--- scripts/descriptions.sh
+++ scripts/descriptions.sh
@@ -8,6 +8,8 @@ xmlfiles="$1/*.clas.xml $1/*.intf.xml"
 
 for xmlfile in $xmlfiles
 do
-  echo "Adding prefix to description in $xmlfile"
-  sed -i '0,/<DESCRIPT>/ s|^\(.*<DESCRIPT>\)\(.*\)\(</DESCRIPT>\)|\1MBT \2\3|g' $xmlfile
+  if [[ -e $xmlfile ]]; then   
+    echo "Adding prefix to description in $xmlfile"
+    sed -i '0,/<DESCRIPT>/ s|^\(.*<DESCRIPT>\)\(.*\)\(</DESCRIPT>\)|\1MBT \2\3|g' $xmlfile
+  fi
 done
