4f55b3721703abeb67bc9acc81352f468ac052be
Marc Bernard <59966492+mbtools@users.noreply.github.com>
2023-05-30 13:32:56 +0200

    Replace exception names

--- scripts/descriptions.sh
+++ scripts/descriptions.sh
@@ -11,5 +11,7 @@ do
   if [[ -e $xmlfile ]]; then   
     echo "Adding prefix to description in $xmlfile"
     sed -i '0,/<DESCRIPT>/ s|^\(.*<DESCRIPT>\)\(.*\)\(</DESCRIPT>\)|\1MBT \2\3|g' $xmlfile
+    sed -i '0,/<OBJ_NAME>/ s|^\(.*<OBJ_NAME>\)ZCX_LOGGER\(</OBJ_NAME>\)|\1/MBTOOLS/CX_LOGGER\2|g' $xmlfile
+    sed -i '0,/<OBJ_NAME>/ s|^\(.*<OBJ_NAME>\)ZCX_LOGGER_DISPLAY_PROFILE\(</OBJ_NAME>\)|\1/MBTOOLS/CX_LOGGER_DISP_PROF\2|g' $xmlfile
   fi
 done
