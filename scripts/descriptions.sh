9a6bcf136516de6e953b5824d5ea395e2bf12dcb
Marc Bernard <59966492+mbtools@users.noreply.github.com>
2024-02-19 12:17:36 +0100

    Fix rename issue

--- scripts/descriptions.sh
+++ scripts/descriptions.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 #
-# Add MBT prefix to descriptions in XML files
+# - Add MBT prefix to descriptions in XML files
+# - Fix rename issue in exception clases
 #
 
 xmlfiles="$1/*.clas.xml $1/*.intf.xml"
@@ -11,7 +12,7 @@ do
   if [[ -e $xmlfile ]]; then   
     echo "Adding prefix to description in $xmlfile"
     sed -i '0,/<DESCRIPT>/ s|^\(.*<DESCRIPT>\)\(.*\)\(</DESCRIPT>\)|\1MBT \2\3|g' $xmlfile
-    sed -i '0,/<OBJ_NAME>/ s|^\(.*<OBJ_NAME>\)ZCX_LOGGER\(</OBJ_NAME>\)|\1/MBTOOLS/CX_LOGGER\2|g' $xmlfile
-    sed -i '0,/<OBJ_NAME>/ s|^\(.*<OBJ_NAME>\)ZCX_LOGGER_DISPLAY_PROFILE\(</OBJ_NAME>\)|\1/MBTOOLS/CX_LOGGER_DISP_PROF\2|g' $xmlfile
+    sed -i 's|^\(.*<OBJ_NAME>\)ZCX_LOGGER\(</OBJ_NAME>\)|\1/MBTOOLS/CX_LOGGER\2|g' $xmlfile
+    sed -i 's|^\(.*<OBJ_NAME>\)ZCX_LOGGER_DISPLAY_PROFILE\(</OBJ_NAME>\)|\1/MBTOOLS/CX_LOGGER_DISP_PROF\2|g' $xmlfile
   fi
 done
